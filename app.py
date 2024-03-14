94d099e5120200f6583f9832d9fae30e62286a95
reisahaveri <reisa.haveri.001@student.uni.lu>
2024-03-14 02:07:32 +0100

    Done with the add function

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+class Calculator:
+    def __init__(self):
+        pass
 
+    def __add__(self, num1, num2):
+        return num1+num2
--- test.py
+++ test.py
@@ -1,1 +1,15 @@
+import pytest
+from app import Calculator
+
+@pytest.fixture()
+def calculator():
+    return Calculator()
+
+# test case for the add method
+def test_add(calculator):
+    assert calculator.add(2,3) == 5
+    assert calculator.add(-1,1) == 0
+    assert calculator.add(0,0) == 0
+    assert calculator.add(-5,-7) == -12
+
 
